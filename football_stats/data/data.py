58b135925417d86256b4400a302695426a8820e1
Antoine Lassallette <antoine_lassallette@msn.com>
2021-11-25 15:59:13 +0100

    new features

--- football_stats/data/data.py
+++ football_stats/data/data.py
@@ -1,13 +1,99 @@
+from numpy import nan
 import pandas as pd
 import requests
 import os
 from dotenv import load_dotenv, find_dotenv
+from datetime import datetime
 
+leagues_ids = [301, 82, 564, 384, 8, 9, 72]
 
 env_path = os.path.join(os.path.dirname(os.path.dirname(__file__)),'.env')
 env_path = find_dotenv()
 load_dotenv(env_path)
 token = os.getenv('API_TOKEN')
+
+def get_season_df():
+    list_of_seasons = []
+    for i in range(1, 4):
+        req = requests.get(
+            f'https://soccer.sportmonks.com/api/v2.0/seasons?api_token={token}&page={i}&per_page=150'
+        )
+        for season in req.json().get('data'):
+            if season.get('league_id') in leagues_ids:
+                list_of_seasons.append([
+                    season.get('id'),
+                    season.get('name'),
+                    season.get('league_id')
+                ])
+    return pd.DataFrame(list_of_seasons,columns=['season_id','name','league_id'])
+
+season_df = get_season_df()
+
+def get_previous_season_id(match):
+    try:
+        row = list(season_df['season_id']).index(match.get('season_id'))
+        return season_df.iloc[(row - 1), :].season_id
+    except:
+        return 'nan'
+
+
+
+def get_standings(match):
+    localteam_id = match.get('localteam_id')
+    visitorteam_id = match.get('visitorteam_id')
+    previous_season_id = get_previous_season_id(match)
+    req = requests.get(
+        f'https://soccer.sportmonks.com/api/v2.0/standings/season/{previous_season_id}?api_token={token}'
+    )
+    try:
+        for team in req.json().get('data')[0].get('standings').get('data'):
+            if team.get('team_id') == localteam_id:
+                localteam_standing = team.get('total').get('points')
+            elif team.get('team_id') == visitorteam_id:
+                visitorteam_standing = team.get('total').get('points')
+        return (localteam_standing, visitorteam_standing)
+    except:
+        return (45, 45)
+
+
+def get_recent_form(match):
+    """given a match returns the localteam and visitorteam recent forms"""
+    localteam_id = match.get('localteam_id')
+    visitorteam_id = match.get('visitorteam_id')
+    season_id = match.get('season_id')
+    localteam_recent_points = 0
+    visitorteam_recent_points = 0
+    req = requests.get(
+        f"https://soccer.sportmonks.com/api/v2.0/standings/season/{season_id}?api_token={token}"
+    )
+    standings = req.json().get('data')[0].get('standings').get('data')
+    nb_teams = len(standings)
+    for i in range(nb_teams):
+        if standings[i].get('team_id') == localteam_id:
+            localteam_recent_form = standings[i].get('recent_form')
+        elif standings[i].get('team_id') == visitorteam_id:
+            visitorteam_recent_form = standings[i].get('recent_form')
+        else:
+            continue
+    try:
+        for char in localteam_recent_form:
+            if char == 'W':
+                localteam_recent_points += 3
+            elif char == 'D':
+                localteam_recent_points += 1
+            else:
+                continue
+        for char in visitorteam_recent_form:
+            if char == 'W':
+                visitorteam_recent_points += 3
+            elif char == 'D':
+                visitorteam_recent_points += 1
+            else:
+                continue
+        return (localteam_recent_points, visitorteam_recent_points)
+    except:
+        return (6, 6)
+
 
 def get_api_responses(token):
     response_pre_change = requests.get(f"https://soccer.sportmonks.com/api/v2.0/fixtures/between/2015-08-01/2020-05-08?api_token={token}&include=lineup.player,substitutions.player&leagues=301,82,564,384,8,9,72&per_page=150&page=1")
@@ -195,10 +281,10 @@ def get_75_lineups(match, lineup_home, lineup_away):
                             lineup_away_75[1]-=1
         return lineup_home_75, lineup_away_75
 
-def get_subs(match, lineup_home, lineup_away):
+def get_90_lineups(match, lineup_home, lineup_away):
     '''
     Getting a match as a dict and its starting lineups,
-    return both lineups updated at 75'
+    return both lineups updated at 90'
     '''
     home_team=str(match.get('localteam_id'))
     lineup=match.get('lineup').get('data')
@@ -261,7 +347,7 @@ def get_all_lineups(matchs):
             lineup_ht_home, lineup_ht_away=get_ht_lineups(match, starting_lineup_home, starting_lineup_away)
             lineup_60_home, lineup_60_away =get_60_lineups(match, lineup_ht_home, lineup_ht_away)
             lineup_75_home, lineup_75_away =get_75_lineups(match, lineup_60_home, lineup_60_away)
-            final_lineup_home, final_lineup_away = get_subs(match, starting_lineup_home, starting_lineup_away)
+            final_lineup_home, final_lineup_away = get_90_lineups(match, starting_lineup_home, starting_lineup_away)
             all_start_lineups.append([starting_lineup_home, starting_lineup_away])
             all_ht_lineups.append([lineup_ht_home, lineup_ht_away])
             all_60_lineups.append([lineup_60_home, lineup_60_away])
@@ -299,18 +385,20 @@ def clean_lineups(list_of_lineups):
                         elif team[x]>6:
                             team[team.index(min(team))]+=2
                             team[x]-=2
-    print(count_error)
     return list_of_lineups
 
 
-def get_game_columns(list_matchs, new_rules='False'):
+def get_game_data(list_matchs):
     '''
     Getting passed a list of dictionnaries for a series of matches,
     return main data for each game as a list of lists
     '''
     game_data=[]
+    count_except=0
     for x in range(len(list_matchs)):
         match = list_matchs[x]
+        H_standings, A_standings = get_standings(match)
+        H_recent_form,A_recent_form=get_recent_form(match)
         try:
             score_ht=[match['scores']['ht_score'][0],match['scores']['ht_score'][2]]
         except:
@@ -337,19 +425,30 @@ def get_game_columns(list_matchs, new_rules='False'):
                 result_ft='D'
         except:
             result_ht='D'
-        if new_rules=='True':
-            if match['league_id']==8:
-                list_to_append=['False', match['id'], match['localteam_id'],match['visitorteam_id'],score_ht, result_ht, score_ft, result_ft]
-            else:
-                list_to_append=['True', match['id'], match['localteam_id'],match['visitorteam_id'],score_ht, result_ht, score_ft, result_ft]
+        if datetime.strptime(
+                match.get('time').get('starting_at').get('date'),
+                "%Y-%m-%d") > datetime(2020, 5, 8):
+            if match['league_id']!=8:
+                list_to_append = [
+                    'True', match['id'], match['localteam_id'],
+                    match['visitorteam_id'], match['season_id'], H_standings,
+                    A_standings, H_recent_form, A_recent_form,score_ht,
+                    result_ht, score_ft, result_ft
+                ]
         else:
-            list_to_append=['False', match['id'], match['localteam_id'],match['visitorteam_id'],score_ht, result_ht, score_ft, result_ft]
+            list_to_append = [
+                'False', match['id'], match['localteam_id'],
+                match['visitorteam_id'], match['season_id'], H_standings,
+                A_standings, H_recent_form, A_recent_form,score_ht, result_ht,
+                score_ft, result_ft
+            ]
         match_list=[]
         match_list.append(list_to_append)
         game_data.append(match_list)
+    print(count_except)
     return game_data
 
-def get_lineups_columns(list_of_game_data, list_of_lineups, vectors=True):
+def get_lineups_columns(list_of_game_data, list_of_lineups, vectors=False):
     '''
     Getting passed a list of game columns and a list of lineups, combine them into one list of lists
     vectors parameters allows you to decide whether you want the data for each lineup as a 3d-vector or as single values
@@ -378,21 +477,31 @@ def get_flatten_rows(list_of_rows):
     return flatten_rows
 
 
-def get_final_df(list_of_flatten_rows, vectors=True):
+def get_final_df(list_of_flatten_rows, vectors=False):
     '''
     Getting passed a list of rows, create a dataframe
     the vectors parameter allows you to choose the adapted columns
     WARNING : the value for vectors argument should be the same for all functions
     '''
     if vectors == True:
-        columns = ['new_rules', 'game_id', 'localteam_id', 'visitorteam_id',"score_ht","result_ht","score_ft","result_ft", 'H_lineup_start', "A_lineup_start", 'H_lineup_ht', "A_lineup_ht", 'H_lineup_60', "A_lineup_60", 'H_lineup_75', "A_lineup_75", 'H_lineup_final', "A_lineup_final"]
+        columns = ['new_rules', 'game_id', 'localteam_id', 'visitorteam_id','season_id',"score_ht","result_ht","score_ft","result_ft", 'H_lineup_start', "A_lineup_start", 'H_lineup_ht', "A_lineup_ht", 'H_lineup_60', "A_lineup_60", 'H_lineup_75', "A_lineup_75", 'H_lineup_final', "A_lineup_final"]
     else:
-        columns=['new_rules', 'game_id', 'localteam_id', 'visitorteam_id',"score_ht","result_ht","score_ft","result_ft","Home_D_start","Home_M_start","Home_A_start","Away_D_start","Away_M_start","Away_A_start","Home_D_ht","Home_M_ht","Home_A_ht","Away_D_ht","Away_M_ht","Away_A_ht","Home_D_60","Home_M_60","Home_A_60","Away_D_60","Away_M_60","Away_A_60","Home_D_75","Home_M_75","Home_A_75","Away_D_75","Away_M_75","Away_A_75","Home_D_final","Home_M_final","Home_A_final","Away_D_final","Away_M_final","Away_A_final"]
+        columns = [
+            'new_rules', 'game_id', 'localteam_id', 'visitorteam_id',
+            'season_id',"H_standings","A_standings","H_recent_form","A_recent_form", "score_ht", "result_ht", "score_ft", "result_ft",
+            "Home_D_start", "Home_M_start", "Home_A_start", "Away_D_start",
+            "Away_M_start", "Away_A_start", "Home_D_ht", "Home_M_ht",
+            "Home_A_ht", "Away_D_ht", "Away_M_ht", "Away_A_ht", "Home_D_60",
+            "Home_M_60", "Home_A_60", "Away_D_60", "Away_M_60", "Away_A_60",
+            "Home_D_75", "Home_M_75", "Home_A_75", "Away_D_75", "Away_M_75",
+            "Away_A_75", "Home_D_final", "Home_M_final", "Home_A_final",
+            "Away_D_final", "Away_M_final", "Away_A_final"
+        ]
     df_lineups = pd.DataFrame(list_of_flatten_rows, columns=columns)
     return df_lineups
 
 
-def collect_data_to_df(list_of_matchs, new_rules='False', vectors=True):
+def collect_data_to_df(list_of_matchs, vectors=False):
     '''
     When passed a list of dictionnaries (each one for a game) and either new rules are true or false,
     calls all others functions to create a dataframe with all tactics and changes for
@@ -401,7 +510,8 @@ def collect_data_to_df(list_of_matchs, new_rules='False', vectors=True):
     the vectors parameter allows you to choose whether you want the data for each lineup as a 3d-vector or as single values
     WARNING : the value for vectors argument should be the same for all functions
     '''
-    game_columns = get_game_columns(list_of_matchs, new_rules)
+
+    game_columns = get_game_data(list_of_matchs)
     list_of_lineups = get_all_lineups(list_of_matchs)
     list_of_lineups=clean_lineups(list_of_lineups)
     columns_with_lineups=get_lineups_columns(game_columns, list_of_lineups, vectors)
